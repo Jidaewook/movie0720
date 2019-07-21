558e1940ccb8470f1ad10d106b58b8a97d78f85b
Jay <psatdoctor@naver.com>
2019-07-21 12:22:38 +0900

    Home Container Part One

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,10 +1,30 @@
 import React, {Component} from "react";
 import MoviesPresenter from "./MoviesPresenter";
+import { movies } from "../../api";
 
 export default class extends Component{
     state = {
-        loading: true
+        loading: true,
+        upcoming: null,
+        popular: null,
+        nowPlaying: null,
+        error: null
     };
+
+    async componentDidMount() {
+        try {
+            const upcoming = await movies.getUpcoming();
+            const popular = await movies.getPopular();
+            const nowPlaying = await movies.getNowPlaying();
+            console.log(upcoming, popular, nowPlaying);
+        } catch{
+            this.setState({ error: "Can't get Movies." });
+
+        } finally {
+            this.setState({ loading: false });
+        }
+    }
+
     render() {
         const { loading } = this.state;
         return <MoviesPresenter loading={loading} />;

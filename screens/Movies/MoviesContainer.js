5e5fa89a0a82361237186cc9a76d45e04365905d
Jay <psatdoctor@naver.com>
2019-07-21 12:33:36 +0900

    Advanced Home & Refactor

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -12,21 +12,44 @@ export default class extends Component{
     };
 
     async componentDidMount() {
+        let upcoming, popular, nowPlaying, error;
+        
         try {
-            const upcoming = await movies.getUpcoming();
-            const popular = await movies.getPopular();
-            const nowPlaying = await movies.getNowPlaying();
-            console.log(upcoming, popular, nowPlaying);
+            // const upcoming = await movies.getUpcoming();
+            // const popular = await movies.getPopular();
+            // const nowPlaying = await movies.getNowPlaying();
+            // console.log(upcoming, popular, nowPlaying);
+
+            ({
+                data: { results: upcoming }
+
+            } = await movies.getUpcoming());
+            ({
+                data: { results: popular }
+
+            } = await movies.getPopular());
+            ({
+                data: { results: nowPlaying }
+
+            } = await movies.getNowPlaying());
+            
+
         } catch{
-            this.setState({ error: "Can't get Movies." });
+            error = "Cant't get Movies.";
 
         } finally {
-            this.setState({ loading: false });
+            this.setState({
+                loading: false,
+                error,
+                upcoming,
+                popular,
+                nowPlaying
+            });
         }
     }
 
     render() {
-        const { loading } = this.state;
+        const { loading, upcoming, popular, nowPlaying } = this.state;
         return <MoviesPresenter loading={loading} />;
     }
 }

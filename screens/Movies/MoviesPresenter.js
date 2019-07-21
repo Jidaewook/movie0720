d91d399521b447f955a2495b02bce8330cc295b3
Jay <psatdoctor@naver.com>
2019-07-21 12:13:19 +0900

    Loading Components(Modified)

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -3,10 +3,10 @@ import { Text } from "react-native";
 import PropTypes from "prop-types";
 import Loader from "../../components/Loader";
 
-const MoviesPresenter = ({loading}) => 
+const MoviesPresenter = ({ loading }) => 
     loading ? <Loader /> : <Text>Movies</Text>;
 
-MoviesPresenter.PropTypes = {
+MoviesPresenter.propTypes = {
     loading: PropTypes.bool.isRequired
 };
 

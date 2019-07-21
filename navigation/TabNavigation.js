aab0319036ca4c3940c9b215459b5d897fa6c04d
Jay <psatdoctor@naver.com>
2019-07-21 11:31:54 +0900

    Modify Navigation/Icons
    tabBar BG Color is operated normally

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,20 +1,77 @@
-import React from "react";
-import {Platform} from "react-native";
+// import React from "react";
+// import {Platform} from "react-native";
 
-import {createBottomTabNavigator, createAppContainer} from "react-navigation";
+// import {createBottomTabNavigator, createAppContainer} from "react-navigation";
+// import MoviesScreen from "../screens/Movies";
+// import TVScreen from "../screens/TV";
+// import SearchScreen from "../screens/Search";
+// import {BG_COLOR} from "../constants/Colors";
+// import TabBarIcon from "../components/TabBarIcon";
+
+// const TabNavigation = createBottomTabNavigator(
+//     {
+//         Movie: {
+//             screen: MoviesScreen,
+//             navigationOptions: {
+//                 tabBarOptions: ({focused}) => (
+//                     <TabBarIcon 
+//                         focused={focused}
+//                         name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+//                     />
+//                 )
+//             }
+//         },
+//         TV: {
+//             screen: TVScreen,
+//             navigationOptions: {
+//                 tabBarOptions: ({ focused }) => (
+//                     <TabBarIcon
+//                         focused={focused}
+//                         name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+//                     />
+//                 )
+//             }
+//         },
+//         Search: {
+//             screen: SearchScreen,
+//             navigationOptions: {
+//                 tabBarOptions: ({ focused }) => (
+//                     <TabBarIcon
+//                         focused={focused}
+//                         name={Platform.OS === "ios" ? "ios-search" : "md-"}
+//                     />
+//                 )
+//             }
+//         }
+//     },
+//     {
+//         tabBarOptions: {
+//             showLabel: false,
+//             style: {
+//                 backgroundColor: BG_COLOR
+//             }
+//         }
+//     }
+// );
+
+// export default createAppContainer(TabNavigation);
+
+import React from "react";
+import { Platform } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import MoviesScreen from "../screens/Movies";
 import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
-import {BG_COLOR} from "../constants/Colors";
-import TabBarIcon from "../Components/TabBarIcon";
+import { BG_COLOR } from "../constants/Colors";
+import TabBarIcon from "../components/TabBarIcon";
 
 const TabNavigation = createBottomTabNavigator(
     {
         Movie: {
             screen: MoviesScreen,
             navigationOptions: {
-                tabBarOptions: ({focused}) => (
-                    <TabBarIcon 
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
                         focused={focused}
                         name={Platform.OS === "ios" ? "ios-film" : "md-film"}
                     />
@@ -24,7 +81,7 @@ const TabNavigation = createBottomTabNavigator(
         TV: {
             screen: TVScreen,
             navigationOptions: {
-                tabBarOptions: ({ focused }) => (
+                tabBarIcon: ({ focused }) => (
                     <TabBarIcon
                         focused={focused}
                         name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
@@ -35,7 +92,7 @@ const TabNavigation = createBottomTabNavigator(
         Search: {
             screen: SearchScreen,
             navigationOptions: {
-                tabBarOptions: ({ focused }) => (
+                tabBarIcon: ({ focused }) => (
                     <TabBarIcon
                         focused={focused}
                         name={Platform.OS === "ios" ? "ios-search" : "md-search"}
